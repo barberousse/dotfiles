a9798b694425df2da38ebe54839675232bde2e54
Benoît Barberousse <benoit@skillsesh.com>
2014-10-17 07:29:47 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,8 +2,10 @@
 
 # Verify Git is installed:
 if [ ! $(which git) ]; then
-  echo "Git is not installed, can't continue."
-  exit 1
+  sudo apt-apt-repository -y ppa:chris-lea/node.js
+  sudo apt-get update
+  sudo apt-get install -y git tmux vim-nox nodejs redis-server \
+    build-essential postgresql-9.3 postgrseql-contrib-9.3 libpq-dev
 fi
 
 # Set .vimrc and .tmux.conf
@@ -21,23 +23,13 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 git clone https://github.com/maljub01/rbenv-bundle-exec.git ~/.rbenv/plugins/rbenv-bundle-exec
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
 # Set gem to skip documentation when installing new gems
 echo 'gem: --no-ri --no-rdoc' >> ~/.gemrc
 
-# Show help if `.rbenv` is not in the path:
-if [ ! $(which rbenv) ]; then
-  echo "
-Seems you still have not added 'rbenv' to the load path:
-
-# ~/.bash_profile:
-
-export RBENV_ROOT=\"\${HOME}/.rbenv\"
-
-if [ -d \"\${RBENV_ROOT}\" ]; then
-  export PATH=\"\${RBENV_ROOT}/bin:\${PATH}\"
-  eval \"\$(rbenv init -)\"
-fi
-"
-fi
+# End with a warning
+echo 'Now source .bashrc'
 
 
