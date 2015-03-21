0635308925bc54da033aee9a801ce94c1b22ca9e
Benoît Barberousse <benoit@skillsesh.com>
2015-03-21 15:43:14 -0700

    Add nvm and rbenv install lines

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
