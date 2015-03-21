f5a3683323ca23f787d8e7d3018835f26d4a4877
Benoît Barberousse <benoit@skillsesh.com>
2015-03-21 16:13:16 -0700

    Add git configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+git config --global credential.helper 'cache --timeout 3600'
+git config --global user.name "Benoît Barberousse"
+git config --global user.email "benoit@skillsesh.com"
